dc5b9538106c2d294b07e1da9ae322d8542ef530
Briggs Elsperger <briggse@protonmail.com>
2018-06-25 04:46:49 -0500

    basic layout and timer

--- public/src/GiftExchange/App.js
+++ public/src/GiftExchange/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route, Link } from 'react-router-dom';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import LandingPage from './containers/LandingPage/LandingPage';
 
 const App = () => (
   <BrowserRouter>
-  <div>
-    <Route path='/' render={() => (<h1>ROOT ROUTE</h1>)}/>
-    <Route path='/test' render={() => (<h1>THIS IS A ROUTE</h1>)}/>
-    <Link to='/test'>TEST</Link>
-  </div>
+  <Layout>
+    <Switch>
+      <Route exact path='/' component={ LandingPage }/>
+      <Route path='/about' render={() => (<h1>AboutRoute</h1>)}/>
+      <Route path='/login' render={() => (<h1>loginRoute</h1>)}/>
+      <Route path='/register' render={() => (<h1>registerRoute</h1>)}/>
+      <Route path='/logout' render={() => (<h1>logoutRoute</h1>)}/>
+      <Route render={() => (<h1>404 Route</h1>)}/>
+    </Switch>
+  </Layout>
   </BrowserRouter>
 );
 
---
+++ public/src/GiftExchange/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+import Header from '../Header/Header';
+
+const Layout = (props) => (
+  <Fragment>
+    <Header />
+    {props.children}
+  </Fragment>
+);
+
+export default Layout;
