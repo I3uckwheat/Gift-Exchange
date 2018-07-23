70eb9d31e2add4008a6da409dab65eb9b90a63c7
Briggs Elsperger <briggse@protonmail.com>
2018-07-22 20:54:01 -0500

    removes router

--- public/src/GiftExchange/App.js
+++ public/src/GiftExchange/App.js
@@ -1,18 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import {  Switch, Route } from 'react-router-dom';
 
-import Classes from "./App.css";
+import Classes from './App.css';
 
 import Left from './Left/Left';
 import Right from './Right/Right';
-
 
 class App extends Component {
   state = {
     user: {
       loggedIn: false
     }
-  }
+  };
 
   render() {
     return (
@@ -20,8 +18,8 @@ class App extends Component {
         <Left />
         <Right />
       </div>
-    )
+    );
   }
-};
+}
 
 export default App;
