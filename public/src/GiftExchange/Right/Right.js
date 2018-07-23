2975b7022626d266fd377be02a743d8f6eb45cee
Briggs Elsperger <briggse@protonmail.com>
2018-07-22 20:54:47 -0500

    adds right panel

--- public/src/GiftExchange/Right/Right.js
+++ public/src/GiftExchange/Right/Right.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import Classes from './Right.css';
 
+import Timer from './Timer/Timer';
+
 const Right = () => (
-  <div className={Classes.Right} />
-)
+  <div className={Classes.Right}>
+    <Timer />
+  </div>
+);
 
 export default Right;
