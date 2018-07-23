14dec073c0613f9d2a471e7b9ca11489c4869020
Briggs Elsperger <briggse@protonmail.com>
2018-07-22 20:54:20 -0500

    adds nav and routes

--- public/src/GiftExchange/Left/Left.js
+++ public/src/GiftExchange/Left/Left.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Classes from './Left.css';
 
 import Header from './Header/Header';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 const Left = () => (
   <div className={Classes.Left}>
     <Header />
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
   </div>
-)
+);
 
 export default Left;
